5c31366f1bdf2617c5ad65538770f53e29d24c88
Way Of The Future <scuzzlebuzzle@yahoo.com>
2016-06-27 16:35:09 -0500

    fix spacing

--- bin/lean-stack.js
+++ bin/lean-stack.js
@@ -34,32 +34,32 @@ program.parse(process.argv);
 var argdir = program.args[0];
 
 if (!argdir) {
-	program.help();
+    program.help();
 }
 else {
-	if (program.stack && !stacktypes[program.stack]) {
-		shell.echo('Invalid stack type.');
-	}
-	else {
-		var stack = stacktypes[program.stack] || 'handlebars';
-		var stackdir = path.join(__dirname, '../stacks/', stack);
+    if (program.stack && !stacktypes[program.stack]) {
+        shell.echo('Invalid stack type.');
+    }
+    else {
+        var stack = stacktypes[program.stack] || 'handlebars';
+        var stackdir = path.join(__dirname, '../stacks/', stack);
 
-		if (!fs.existsSync(stackdir)) {
-			shell.echo('This stack is currently under construction.');
-			shell.echo('Please fork our repository and contribute on GitHub!');
-			shell.echo('https://github.com/wayofthefuture/lean-stack/fork');
-		}
-		else {
-		    var src  = path.join(stackdir, '/*');
-			var dest = path.resolve(argdir);
+        if (!fs.existsSync(stackdir)) {
+            shell.echo('This stack is currently under construction.');
+            shell.echo('Please fork our repository and contribute on GitHub!');
+            shell.echo('https://github.com/wayofthefuture/lean-stack/fork');
+        }
+        else {
+            var src  = path.join(stackdir, '/*');
+            var dest = path.resolve(argdir);
 
-			if (!fs.existsSync(dest)) {
-				shell.mkdir('-p', dest);
-			}
+            if (!fs.existsSync(dest)) {
+                shell.mkdir('-p', dest);
+            }
 
-		    shell.echo('Generating ' + stack + ' in ' + dest + '...');
+            shell.echo('Generating ' + stack + ' in ' + dest + '...');
             shell.cp('-r', src, dest);
             shell.echo(shell.error() ? 'An error has occurred.' : 'Done!');
-		}
-	}
+        }
+    }
 }
